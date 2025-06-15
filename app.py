17b39bcd3322b979cfeab39bda3ceb1906bac60f
Nikhilesh Garipally <63578275+nikilesh-garip@users.noreply.github.com>
2025-06-15 12:20:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("🤖 Chat with Hugging Face Model")
 
 @st.cache_resource
 def load_model():
-    model_name = "tiiuae/falcon-rw-1b"
+    model_name = "microsoft/DialoGPT-small"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
     return tokenizer, model
@@ -16,7 +16,7 @@ tokenizer, model = load_model()
 chat_input = st.text_input("You:", "")
 
 if chat_input:
-    inputs = tokenizer(chat_input, return_tensors="pt")
+    inputs = tokenizer(chat_input + tokenizer.eos_token, return_tensors="pt")
     with st.spinner("Thinking..."):
         reply_ids = model.generate(**inputs, max_new_tokens=100, pad_token_id=tokenizer.eos_token_id)
     reply = tokenizer.decode(reply_ids[0], skip_special_tokens=True)
